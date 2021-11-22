eaf992afd237bb3096e94f1851f70eb3950fe82e
Jacob Keller <70432484+jkeller52@users.noreply.github.com>
2021-11-22 15:44:36 -0500

    Update Resume.js

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -25,7 +25,7 @@ const sections = [
 const Resume = () => (
   <Main
     title="Resume"
-    description="Michael D'Angelo's Resume. Arthena, Matroid, YC, Skeptical Investments, Stanford ICME, Planet Labs, and Facebook."
+    description="Jacob Kellere's Resume. Ohio State University, Resilint Cognitive Solutions."
   >
     <article className="post" id="resume">
       <header>
