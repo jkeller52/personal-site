d5cf294ea158757b754da7ae4937342b898508c2
Jacob Keller <70432484+jkeller52@users.noreply.github.com>
2021-11-23 03:05:03 -0500

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,10 +1,35 @@
 // TODO Add a couple lines about each project
 const data = [
-  {
-    title: 'Nearest Dollar',
-    subtitle: '2015 BVP Hackathon',
+
+   {
+    title: 'A Critical Examination of Autonomous Flight Safety Systems from a Cognitive Systems Engineering Perspective: Challenges, Themes, and Outlying Risks',
+    link: 'https://journals.sagepub.com/doi/abs/10.1177/1071181321651248',
+    subtitle: 'Presented October 6 at the Human Factors and Ergonomics Society Annual Meeting',
     image: '/images/projects/nearestdollar.jpg',
-    date: '2015-11-20',
+    date: '2021-10-06',
+    desc:
+      'Keller, J., IJtsma, M., & Newton, E. K. (2021) A Critical Examination of Autonomous Flight Safety Systems from a Cognitive Systems Engineering Perspective: Challenges, Themes, and Outlying Risks Jacob Keller. International Astronautical Congress, October, 25–29. '
+      + 'NearestDollar connected to your bank accounts, credit cards, '
+      + 'or debit cards and rounded up your purchases to donate the balance to '
+      + 'the charity of your choice.',
+  }, 
+  {
+    title: 'Computational Approaches to Analyzing Resilience in Human-Machine Teams',
+    link: 'https://journals.sagepub.com/doi/abs/10.1177/1071181321651248',
+    subtitle: 'Presented October 6 at the Human Factors and Ergonomics Society Annual Meeting',
+    image: '/images/projects/nearestdollar.jpg',
+    date: '2021-10-06',
+    desc:
+      'Keller, J., & IJtsma, M. (2021). Requirements For Computational Approaches To Analyzing Resilience In Human-Machine Teams. Human Factors and Ergonomics Society. Presented 10/6/2021 in Baltimore, Maryland, at the Human Factors and Ergonomics Society 65th Annual Meeting. https://doi.org/10.1177/1071181321651248'
+      + 'NearestDollar connected to your bank accounts, credit cards, '
+      + 'or debit cards and rounded up your purchases to donate the balance to '
+      + 'the charity of your choice.',
+  },
+  {
+    title: 'Astrophotography in the Dominican Republic',
+    subtitle: 'April 2021',
+    image: '/images/projects/nearestdollar.jpg',
+    date: '2021-04-27',
     desc:
       'Built for a social impact hackathon. '
       + 'NearestDollar connected to your bank accounts, credit cards, '
@@ -12,18 +37,29 @@ const data = [
       + 'the charity of your choice.',
   },
   {
-    title: 'Harvest',
+    title: 'Autonomous Flight Safety Systems Student Project',
     subtitle: 'Won 3rd. place in 2015 Techcrunch Disrupt SF Hackathon',
     link: 'https://devpost.com/software/harvest',
     image: '/images/projects/harvest.jpg',
-    date: '2015-09-20',
+    date: '2020-05-01',
     desc:
       'Won ~ $7000 in prizes for an advanced, low cost monitoring solution '
       + 'for crops. Harvest was designed to catch irrigation leaks, overwatering, '
       + 'and nutrient deficiencies at an affordable price for the developing world.',
   },
   {
-    title: 'Space Potato',
+    title: 'piGUI',
+    subtitle: 'A beginner friendly IoT Touchscreen device using Python and shell scripts.',
+    image: '/images/projects/nearestdollar.jpg',
+    date: '2021-01-20',
+    desc:
+      'Built for a social impact hackathon. '
+      + 'NearestDollar connected to your bank accounts, credit cards, '
+      + 'or debit cards and rounded up your purchases to donate the balance to '
+      + 'the charity of your choice.',
+  },
+  {
+    title: 'Software Resilience Engineering Literature Search',
     subtitle: 'A kickstarter funded potato powered weather balloon.',
     link: 'http://www.spacepotato.org',
     image: '/images/projects/spacepotato.jpg',
@@ -32,16 +68,6 @@ const data = [
       'Launched a potato battery powered weather balloon with two cameras '
       + 'and gps transponder. Resulting photos were published in a coffee table book. '
       + 'You can email me for a copy.',
-  },
-  {
-    title: 'Cat Detector',
-    subtitle: 'A convolutional neural network to classify cats! (and dogs)',
-    image: '/images/projects/catdetector.jpg',
-    date: '2015-05-15',
-    desc:
-      'Trained a convolutional neural network to classify between ~ 80 cats breeds. '
-      + 'Over 60,000 cats were classified before server bills made the project too expensive '
-      + 'to continue hosting.',
   },
 ];
 
