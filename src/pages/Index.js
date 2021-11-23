4d8c2e9cdcc5a00fe30a03967cf6f6be347a6e22
Jacob Keller <70432484+jkeller52@users.noreply.github.com>
2021-11-23 02:51:03 -0500

    Update Index.js

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -10,15 +10,14 @@ const Index = () => (
     <article className="post" id="index">
       <header>
         <div className="title">
-          <h2 data-testid="heading"><Link to="/">About this site</Link></h2>
+          <h2 data-testid="heading"><Link to="/">Welcome to my site</Link></h2>
           <p>
-            A beautiful, responsive, statically-generated,
-            react application written with modern Javascript.
+            This is my personal and professional page with info about what I am up to
           </p>
         </div>
       </header>
-      <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
-        or you can check out my {' '}
+      <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>.
+        You can also check out my {' '}
         <Link to="/resume">resume</Link>, {' '}
         <Link to="/projects">projects</Link>, {' '}
         view <Link to="/stats">site statistics</Link>, {' '}
