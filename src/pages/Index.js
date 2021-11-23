b7a3ee9a1f50eac2f168cf37f9cb3108a5e709ea
Jacob Keller <70432484+jkeller52@users.noreply.github.com>
2021-11-23 12:56:28 -0500

    Update Index.js

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import ReactMarkdown from 'react-markdown';
 import Main from '../layouts/Main';
 
 // uses babel to load contents of file
 const markdown = raw('../pages/Index.md');
-import ReactMarkdown from 'react-markdown';
+
 const count = markdown.split(/\s+/)
   .map((s) => s.replace(/\W/g, ''))
   .filter((s) => s.length).length;
