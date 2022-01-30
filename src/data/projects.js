d603c7e01a7ae1b65f85169d7039ceaadcf5ea1c
keller.974 <keller.974@osu.edu>
2022-01-29 21:57:04 -0500

    edited projects.js
    Owq
    wq

--- src/data/projects.js
+++ src/data/projects.js
@@ -9,9 +9,18 @@ const data = [
     date: '2021-10-06',
     desc:
       'Keller, J., IJtsma, M., & Newton, E. K. (2021) A Critical Examination of Autonomous Flight Safety Systems from a Cognitive Systems Engineering Perspective: Challenges, Themes, and Outlying Risks Jacob Keller. International Astronautical Congress, October, 25–29. '
-      + 'NearestDollar connected to your bank accounts, credit cards, '
-      + 'or debit cards and rounded up your purchases to donate the balance to '
-      + 'the charity of your choice.',
+        + 'A student project funded by the Federal Aviation Administration (FAA) aimed identify'
+        + 'complexities, regulatory challenges, and emerging issues '
+        + 'surrounding autononomous flight safety systems (AFSS) for commercial space launches.'
+        + 'Interviews were conducted with over eighty commerical stakeholders in government and industry, culminating'
+        + 'in a whitepaper including both technical and policy recommendations.  '
+        + ''
+        + 'Throughout the scope of this project, I noticed interesting connections between topics in cognitive systems'
+        + 'engineering and challenges/themes about AFSS. This sparked interest in writing a research paper based on'
+        + 'information collected in interviews. The paper applies a transdisciplinary approach, providing background and'
+        + 'context on autonomous flight safety systems before identifying key assumptions and engineering tradeoffs in its design'
+        + '',
+        
   },
   {
     title: 'Computational Approaches to Analyzing Resilience in Human-Machine Teams',
@@ -21,20 +30,26 @@ const data = [
     date: '2021-10-06',
     desc:
       'Keller, J., & IJtsma, M. (2021). Requirements For Computational Approaches To Analyzing Resilience In Human-Machine Teams. Human Factors and Ergonomics Society. Presented 10/6/2021 in Baltimore, Maryland, at the Human Factors and Ergonomics Society 65th Annual Meeting. https://doi.org/10.1177/1071181321651248'
-      + 'NearestDollar connected to your bank accounts, credit cards, '
-      + 'or debit cards and rounded up your purchases to donate the balance to '
-      + 'the charity of your choice.',
+        + 'Human-machine teams (HMTs) in complex work domains need to be able to adapt to variable and uncertain work demands.'
+        + 'Computational modeling and simulation can provide novel approaches to the evaluation of HMTs'
+        + 'performing complex joint activities, affording large-scale, quantitative analysis of team characteristics'
+        + '(such as system architecture and governance protocols) and their effects on resilience. Drawing from literature'
+        + 'in resilience engineering, human-automation interaction, and cognitive systems engineering, this paper provides'
+        + 'a theoretical exploration of the use of computational modeling and simulation to analyze resilience in HMTs.'
+        + 'Findings from literature are summarized in a set of requirements that highlight key aspects of resilience in'
+        + 'HMTs that need to be accounted for in future modeling and evaluation efforts.',
   },
   {
     title: 'Astrophotography in the Dominican Republic',
-    subtitle: 'April 2021',
+    subtitle: 'April 2021 - Sony a6000',
     image: '/images/projects/nearestdollar.jpg',
     date: '2021-04-27',
     desc:
       'Built for a social impact hackathon. '
-      + 'NearestDollar connected to your bank accounts, credit cards, '
-      + 'or debit cards and rounded up your purchases to donate the balance to '
-      + 'the charity of your choice.',
+      + 'Stats:'
+      + 'Camera: Sony a6000'
+      + 'Lens:NEEWER 1.7f/35MM',
+      + 'Approximate Coordinates: '18°22'20.8"N 68°50'29.7"W''
   },
   {
     title: 'Autonomous Flight Safety Systems Student Project',
